114f5a147d17aa3f74bc1df1ac8e2bfae0c8fc91
Mariusz Skoneczko <mariuszskon@users.noreply.github.com>
2017-02-11 15:59:54 +1100

    Moved pin number definitions to pins.h

---
+++ pins.h
@@ -0,0 +1,23 @@
+#ifndef PINS_H
+#define PINS_H
+
+// LCD
+#define RS_PIN 7
+#define EN_PIN 8
+#define D4_PIN 9
+#define D5_PIN 10
+#define D6_PIN 11
+#define D7_PIN 12
+
+// PIEZO
+#define PIEZO_PIN 6
+
+// BUTTONS
+#define BUTTON0_PIN 2
+#define BUTTON1_PIN 3
+#define BUTTON2_PIN 4
+
+// UNCONNECTED ANALOG
+#define UNCONNECTED_ANALOG A0
+
+#endif
